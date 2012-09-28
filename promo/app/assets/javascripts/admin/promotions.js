138efa040267e45d8d27e2c71f767b06428b20a7
Ryan Bigg <radarlistener@gmail.com>
2012-09-28 16:44:55 +1000

    WIP: Refactor product autocomplete
    This is refactoring product autocomplete to use the select2 plugin that some other parts of Spree depend on.

--- promo/app/assets/javascripts/admin/promotions.js
+++ promo/app/assets/javascripts/admin/promotions.js
@@ -1,7 +1,5 @@
 var initProductActions = function(){
 
-  $(".product_autocomplete").product_autocomplete();
-
   $('.calculator-fields').each(function(){
     var $fields_container = $(this);
     var $type_select = $fields_container.find('.type-select');
